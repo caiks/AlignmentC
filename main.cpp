d0f745c3ff35e9a86f93a9815b33da849686cf0f
Cliff McCartney <caiks@greenlake.co.uk>
2019-07-26 16:03:17 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,4 +15,7 @@ void main()
     VarPair v3(new VarStr("aa"), new VarInt(3));
     cout << v3 << endl;
 
+    VarPair v4(new VarStr("aa"), new VarPair(new VarStr("aa"), new VarInt(3)));
+    cout << v4 << endl;
+
 }
