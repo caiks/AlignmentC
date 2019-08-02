9eb4803ef99f05759ea3e63e49a33672278a0f13
Cliff McCartney <caiks@greenlake.co.uk>
2019-08-02 12:26:09 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "Alignment.h"
 #include <iostream>
+#include <set>
+#include <unordered_set>
 
 using namespace Alignment;
 using namespace std;
@@ -56,6 +58,24 @@ void main()
     cout << "v4 < v5 == " << (v4 < v5) << endl;
     cout << "v4 > v5 == " << (v4 > v5) << endl;
     cout << "v4 == v5 == " << (v4 == v5) << endl;
+
+    set<Var> vv1 = { v1,v2,v3,v4,v5 };
+    cout << "vv1 =";
+    for (auto v : vv1)
+	cout << " " << v;
+    cout << endl;
+
+    set<Var> vv2 = { v5,v4,v3,v2,v1 };
+    cout << "vv2 =";
+    for (auto v : vv2)
+	cout << " " << v;
+    cout << endl;
+
+    unordered_set<Var> vv3 = { v5,v4,v3,v2,v1 };
+    cout << "vv3 =";
+    for (auto v : vv3)
+	cout << " " << v;
+    cout << endl;
 
 
 }
