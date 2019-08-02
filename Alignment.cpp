956201830f8752364f4e69f661a0a99c518a26bc
Cliff McCartney <caiks@greenlake.co.uk>
2019-08-02 10:26:21 +0100

    Add files via upload

--- Alignment.cpp
+++ Alignment.cpp
@@ -2,40 +2,4 @@
 
 using namespace Alignment;
 
-Var::Var(const std::string& s)
-{
-    cl = 1;
-    rep_str = s;
-}
-
-
-Var::Var(int i)
-{
-    cl = 2;
-    rep_int = i;
-}
-
-Var::Var(const Var& v0, const Var& v1)
-{
-    cl = 5;
-    rep0 = std::make_shared<Var>(v0);
-    rep1 = std::make_shared<Var>(v1);
-}
-
-std::ostream& Alignment::operator<<(std::ostream& stream, const Var& v) {
-    switch (v.cl) {
-    case 1: 
-	stream << v.rep_str;
-	break;       
-    case 2: 
-	stream << v.rep_int;
-	break;
-    case 5: 
-	stream << "<" << *v.rep0  << "," << *v.rep1 << ">";
-	break;
-    default:
-	throw std::runtime_error("std::ostream& operator<<(std::ostream&, const Var&) fail");
-    }
-    return stream;
-}
 
--- Alignment.h
+++ Alignment.h
@@ -7,17 +7,61 @@
 
 namespace Alignment
 {
-    struct Var
+    class Var
     {
-	Var(const std::string&);
-	Var(int);
-	Var(const Var&, const Var&);
-	int cl;
-	std::string rep_str;
-	int rep_int;
-	std::shared_ptr<Var> rep0;
-	std::shared_ptr<Var> rep1;
-	friend std::ostream& operator<<(std::ostream&, const Var&);
+	private: Var() = default;
+
+	public: Var(const Var& _v)
+	{
+	    this->cl = _v.cl;
+	    this->rep_str = _v.rep_str;
+	    this->rep_int = _v.rep_int;
+	    this->rep_var0 = _v.rep_var0;
+	    this->rep_var1 = _v.rep_var1;
+	}
+
+	public: Var(const std::string& _s)
+	{
+	    this->cl = 1;
+	    this->rep_str = _s;
+	}
+
+	public: Var(int _i)
+	{
+	    this->cl = 2;
+	    this->rep_int = _i;
+	}
+
+	public: Var(const Var& _v0, const Var& _v1)
+	{
+	    this->cl = 5;
+	    this->rep_var0 = std::make_shared<Var>(_v0);
+	    this->rep_var1 = std::make_shared<Var>(_v1);
+	}
+
+	public: friend std::ostream& operator<<(std::ostream& _out, const Var& _v)
+	{
+	    switch (_v.cl) {
+	    case 1:
+		_out << _v.rep_str;
+		break;
+	    case 2:
+		_out << _v.rep_int;
+		break;
+	    case 5:
+		_out << "<" << *_v.rep_var0 << "," << *_v.rep_var1 << ">";
+		break;
+	    default:
+		throw std::logic_error("std::ostream& operator<<(std::ostream&, const Var&)");
+	    }
+	    return _out;
+	}
+
+	private: char cl;
+	private: std::string rep_str;
+	private: int rep_int;
+	private: std::shared_ptr<Var> rep_var0;
+	private: std::shared_ptr<Var> rep_var1;
     };
 }
 #endif
